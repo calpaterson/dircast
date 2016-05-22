54ef2706464fae079dafe28f6bfb7d3d21e1f9c0
Cal Paterson <cal@calpaterson.com>
2016-05-22 14:25:52 +0100

    Log to stderr, add a debug option

--- dircast/cli.py
+++ dircast/cli.py
@@ -1,16 +1,19 @@
-from logging import basicConfig, INFO, getLogger
+from logging import basicConfig, INFO, ERROR, getLogger
 from pathlib import Path
+from sys import stderr
 
 import click
 
-from dircast.files import find_files
+from dircast.files import load_channel_file, find_files
 
 @click.command()
 @click.argument(
     "directory",
 )
-def main(directory):
-    basicConfig(level=INFO)
+@click.option("--debug", is_flag=True, default=False, type=bool)
+def main(directory, debug):
+    basicConfig(level=INFO if debug else ERROR, steam=stderr)
     getLogger(__name__).info("started")
+    load_channel_file(Path(directory))
     for file in find_files(Path(directory)):
         pass
